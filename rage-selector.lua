362868875027bc84b1da27f35d5bee849326d70e
ronaldos1337 <130254939+ronaldos1337@users.noreply.github.com>
2023-05-12 01:34:36 +0200

    Add files via upload

---
+++ rage-selector.lua
@@ -0,0 +1,16 @@
+local logs = {}
+local fonts = nil
+
+local hitgroupMap = {
+    [0] = 'generic',
+    [1] = 'head',
+    [2] = 'chest',
+    [3] = 'stomach',
+    [4] = 'left arm',
+    [5] = 'right arm',
+    [6] = 'left leg',
+    [7] = 'right leg',
+    [8] = 'neck',
+    [9] = 'gear'
+  }
+end
